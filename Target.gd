12613c9381e3b32e0cd8e93b0190900c509b4256
Jakeboiegg <jake_ling_kae_eun@s2021.ssts.edu.sg>
2024-11-15 15:57:41 +0800

    initial commit

---
+++ Target.gd
@@ -0,0 +1,22 @@
+extends Node
+@onready var collision = get_node("Area2D/CollisionShape2D")
+@onready var target = get_node(".")
+var mouse_top = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if mouse_top:
+		target.play("blue")
+	else:
+		target.play("red")
+	
+func _on_area_2d_mouse_entered():
+	print("entered!!")
+	mouse_top = true
+	
+func _on_area_2d_mouse_exited():
+	print("exited!!")
+	mouse_top = false
