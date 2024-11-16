5714fafba6cf304a67a0f88b07e8a012bab3c60a
Belvis <jake_ling_kae_eun@s2021.ssts.edu.sg>
2024-11-16 18:08:15 +0800

    got rid of print statements from target scene

--- target.gd
+++ target.gd
@@ -9,16 +9,13 @@ func _ready():
 	pass # Replace with function body.
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	print(mouse_top)
 	if mouse_top:
 		target.play("blue")
 	else:
 		target.play("red")
 	
 func _on_area_2d_mouse_entered():
-	print("entered!!")
 	mouse_top = true
 	
 func _on_area_2d_mouse_exited():
-	print("exited!!")
 	mouse_top = false
