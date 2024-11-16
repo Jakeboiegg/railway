04d4d23eba41fbd1af24b21773b4e18c1a0a2dc8
Belvis <jake_ling_kae_eun@s2021.ssts.edu.sg>
2024-11-16 20:48:12 +0800

    suitcase ui wireframe

---
+++ suitcase.gd
@@ -0,0 +1,16 @@
+extends Node
+
+@onready var suitcase_ui: ReferenceRect = $suitcase_texture_ref
+var inventory = false
+
+func _init() -> void:
+	suitcase_ui.visible = false
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("toggle_inventory"):
+		inventory = not inventory
+		
+	if inventory:
+		suitcase_ui.visible = true
+	else:
+		suitcase_ui.visible = false
