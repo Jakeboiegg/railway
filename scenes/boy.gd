f9859531beacfb8910a3da0e3d00fc8b85e4a3c9
Belvis <jake_ling_kae_eun@s2021.ssts.edu.sg>
2024-11-16 16:20:15 +0800

    added boy (with bad animations)

--- scenes/boy.gd
+++ scenes/boy.gd
@@ -1,17 +1,15 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 50
 const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-
-func _physics_process(delta):
+func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity += get_gravity() * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -19,10 +17,20 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	# animations
+	if direction == 0:
+		animated_sprite.play("idle")
+	elif direction > 0:
+		animated_sprite.flip_h = false
+		animated_sprite.play("walk")
+	else:
+		animated_sprite.flip_h = true
+		animated_sprite.play("walk")
+		
 	move_and_slide()
